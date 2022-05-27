3f577f4bc77c103b88dd8af7dbe140d4f9d1918d
husen <1178515826@qq.com>
2022-05-27 10:32:09 +0800

    support-webflux

---
+++ domain-security/security-interfaces/src/main/java/com/jsen17/security/interfaces/DomainSecurityApp.java
@@ -0,0 +1,16 @@
+package com.jsen17.security.interfaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HuSen
+ * @since 2022/5/27 10:31 AM
+ */
+@SpringBootApplication
+public class DomainSecurityApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomainSecurityApp.class, args);
+    }
+}
